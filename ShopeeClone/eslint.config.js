04521474bc430f567dbc2d41d77d441e6f491ed7
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2025-04-19 14:04:53 +0700

    Cài đặt dự án: - Cài đặt môi trường: ReactJs Typescript với Vite - Cài đặt ESlint, Prettier và Tailwindcss

---
+++ ShopeeClone/eslint.config.js
@@ -0,0 +1,70 @@
+// eslint.config.js
+import eslintPluginPrettier from 'eslint-plugin-prettier'
+import js from '@eslint/js'
+import globals from 'globals'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import tseslint from 'typescript-eslint'
+
+export default [
+  {
+    ignores: ['dist', 'vite.config.ts', 'node_modules']
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+      parserOptions: {
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+      'jsx-a11y': jsxA11y,
+      prettier: eslintPluginPrettier
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      },
+      'import/resolver': {
+        node: {
+          paths: ['.'],
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }
+      }
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': 'warn',
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ]
+    }
+  }
+]
