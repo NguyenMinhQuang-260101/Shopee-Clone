d7ca189a9dd972716a040d37c8ea9280687de31c
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2025-04-21 11:41:26 +0700

    Code UI trang Login / Register

---
+++ ShopeeClone/src/pages/Login/Login.tsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom'
+
+export default function Login() {
+  return (
+    <div className='bg-orange lg:bg-[url("https://down-vn.img.susercontent.com/file/sg-11134004-7rd4w-m7m4ydo2e4ljf1")] lg:bg-no-repeat lg:bg-center lg:bg-contain bg-none'>
+      <div className='max-w-7xl mx-auto px-4'>
+        <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='p-10 rounded bg-white shadow-sm'>
+              <div className='text-2xl'>Đăng nhập</div>
+              <div className='mt-8'>
+                <input
+                  type='email'
+                  name='email'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-md'
+                  placeholder='Email'
+                />
+                <div className='mt-1 text-[#ff424f] min-h-[1rem] text-sm'></div>
+              </div>
+              <div className='mt-3'>
+                <input
+                  type='password'
+                  name='password'
+                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-md'
+                  placeholder='Password'
+                />
+                <div className='mt-1 text-[#ff424f] min-h-[1rem] text-sm'></div>
+              </div>
+              <div className='mt-3'>
+                <button className='w-full bg-orange text-white p-3 rounded-sm mt-4 hover:bg-orange/80 uppercase'>
+                  đăng nhập
+                </button>
+              </div>
+              <div className='mt-8 flex items-center justify-center'>
+                <span className='text-gray-300'>Bạn chưa có tài khoản?</span>
+                <Link to='/register' className='ml-1 text-orange cursor-pointer hover:text-orange/80'>
+                  Đăng ký
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
