f2e99c07f59e9d4b5073e3bcd8b5acddf9443cd9
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2025-04-22 13:24:32 +0700

    Khai báo interface và thực hiện gọi API Register

--- ShopeeClone/src/pages/Register/Register.tsx
+++ ShopeeClone/src/pages/Register/Register.tsx
@@ -4,6 +4,9 @@ import { schema } from '../../utils/rules'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { FormData } from '../../types/register.type'
 import Input from '../../components/Input'
+import { useMutation } from '@tanstack/react-query'
+import { omit } from 'lodash'
+import { registerAccount } from '../../apis/auth.api'
 
 export default function Register() {
   const {
@@ -16,14 +19,25 @@ export default function Register() {
     resolver: yupResolver(schema)
   })
 
-  const onSubmit = handleSubmit((data) => {
-    // console.log(data)
-    // const password = getValues('password')
-    // const confirmPassword = getValues('confirm_password')
+  const registerAccountMutation = useMutation({
+    mutationFn: (body: Omit<FormData, 'confirm_password'>) => registerAccount(body)
   })
 
-  // const password = watch('password')
-  // console.log('password', password)
+  const onSubmit = handleSubmit((data) => {
+    // * Cách 1: Dùng destructuring
+    // const { confirm_password, ...body } = data
+    // registerAccountMutation.mutate(body)
+    // * Cách 2: Dùng lodash omit
+    const body = omit(data, ['confirm_password'])
+    registerAccountMutation.mutate(body, {
+      onSuccess: (data) => {
+        console.log('Register successfully', data)
+      },
+      onError: (error) => {
+        console.log('Register failed', error)
+      }
+    })
+  })
 
   return (
     <div className='bg-orange lg:bg-[url("https://down-vn.img.susercontent.com/file/sg-11134004-7rd4w-m7m4ydo2e4ljf1")] lg:bg-no-repeat lg:bg-center lg:bg-contain bg-none'>
