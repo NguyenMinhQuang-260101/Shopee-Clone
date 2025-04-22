757ce921577941d24546ba2767f3edd4cad50776
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2025-04-22 13:55:51 +0700

    Xử lý lỗi 422 từ server trả về

--- ShopeeClone/src/pages/Register/Register.tsx
+++ ShopeeClone/src/pages/Register/Register.tsx
@@ -7,6 +7,8 @@ import Input from '../../components/Input'
 import { useMutation } from '@tanstack/react-query'
 import { omit } from 'lodash'
 import { registerAccount } from '../../apis/auth.api'
+import { isAxiosUnprocessableEntityError } from '../../utils/utils'
+import { ResponseApi } from '../../types/utils.type'
 
 export default function Register() {
   const {
@@ -14,6 +16,7 @@ export default function Register() {
     handleSubmit,
     // watch,
     // getValues,
+    setError,
     formState: { errors }
   } = useForm<FormData>({
     resolver: yupResolver(schema)
@@ -34,7 +37,32 @@ export default function Register() {
         console.log('Register successfully', data)
       },
       onError: (error) => {
-        console.log('Register failed', error)
+        if (isAxiosUnprocessableEntityError<ResponseApi<Omit<FormData, 'confirm_password'>>>(error)) {
+          const formError = error.response?.data.data
+          // * Cách 1: Dùng forEach trong trường hợp có nhiều trường dữ liệu cần kiểm tra lỗi
+          if (formError) {
+            const formErrorKeys = Object.keys(formError) as Array<keyof Omit<FormData, 'confirm_password'>>
+            formErrorKeys.forEach((key) => {
+              setError(key, {
+                message: formError[key],
+                type: 'Server'
+              })
+            })
+          }
+          // * Cách 2: Dùng destructuring trong trường hợp chỉ có 1 số ít trường dữ liệu cần kiểm tra lỗi
+          // if (formError?.email) {
+          //   setError('email', {
+          //     message: formError.email,
+          //     type: 'Server'
+          //   })
+          // }
+          // if (formError?.password) {
+          //   setError('password', {
+          //     message: formError.password,
+          //     type: 'Server'
+          //   })
+          // }
+        }
       }
     })
   })
