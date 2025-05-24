b8619efebc2e42f21b770c9664a83d41676c6d4f
NguyenMinhQuang-260101 <quangnguyenminh2001@gmail.com>
2025-05-24 14:12:28 +0700

    Test lỗi nhập data không đúng định dạng trên form

--- ShopeeClone/src/pages/Login/Login.test.tsx
+++ ShopeeClone/src/pages/Login/Login.test.tsx
@@ -1,20 +1,52 @@
-import { describe, expect, it } from 'vitest'
+import { beforeAll, describe, expect, it } from 'vitest'
 import { logScreen, renderWithRouter } from '../../utils/testUtils'
 import path from '../../constants/path'
-import { screen, waitFor } from '@testing-library/react'
+import { fireEvent, screen, waitFor } from '@testing-library/react'
 
 describe('Login', () => {
-  it('Hiển thị lỗi required khi không nhập email', async () => {
-    const { user } = renderWithRouter({ route: path.login })
+  //* Trong một số trường hợp bị mutiple 1 số thẻ nguyên nhân là dùng beforeEach khiến cho render chồng chéo làm nhân ra nhieu lần
+  //* Ta dùng beforeAll để render 1 lần duy nhất
+  beforeAll(async () => {
+    renderWithRouter({ route: path.login })
     await waitFor(() => {
       expect(screen.queryByPlaceholderText('Email')).not.toBeNull()
     })
+  })
+  it('Hiển thị lỗi requited không nhập gì', async () => {
     const submitButton = screen.getByRole('button', {
       name: /Đăng nhập/i
     })
-    user.click(submitButton)
-    expect(await screen.findByText('Vui lòng nhập email')).toBeTruthy()
-    expect(await screen.findByText('Vui lòng nhập mật khẩu')).toBeTruthy()
+    fireEvent.submit(submitButton)
+    // await waitFor(async () => {
+    //   expect(await screen.queryByText('Vui lòng nhập email')).not.toBeNull()
+    //   expect(await screen.queryByText('Vui lòng nhập mật khẩu')).not.toBeNull()
+    // })
+    await waitFor(() => {
+      expect(screen.queryByText('Vui lòng nhập email')).not.toBeNull()
+      expect(screen.queryByText('Vui lòng nhập mật khẩu')).not.toBeNull()
+    })
+  })
+  it('Hiển thị lỗi nhập email và password không hợp lệ', async () => {
+    // const emailInput = screen.getByRole('textbox', { name: /email/i })
+    // const passwordInput = screen.getByRole('textbox', { name: /password/i })
+    const emailInput = document.querySelector('form input[type="email"]') as Element
+    const passwordInput = document.querySelector('form input[type="password"]') as Element
+    const submitButton = screen.getByRole('button', {
+      name: /Đăng nhập/i
+    })
+    fireEvent.change(emailInput, {
+      target: {
+        value: 'testgmail.com'
+      }
+    })
+    fireEvent.change(passwordInput, {
+      target: {
+        value: '123'
+      }
+    })
+    fireEvent.submit(submitButton)
+    expect(await screen.findByText('Email không hợp lệ')).toBeTruthy()
+    expect(await screen.findByText('Mật khẩu không được ngắn hơn 6 ký tự')).toBeTruthy()
     await logScreen('Render trang Login')
   })
 })
